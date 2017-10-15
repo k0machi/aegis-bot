e2a8d8aa75ea0b16cb7550603895ac4a6a253819
arisv <inquizgf@gmail.com>
2017-10-15 21:36:14 +0300

    Print on image, a feature nobody asked for

---
+++ commands/printimage.js
@@ -0,0 +1,52 @@
+﻿module.exports.exec = async (bot, message, args) => {
+    var jimp = require('jimp');
+    var fileName = './resources/printimage/sign.png';
+    var processedImage;
+    var textToPrint = args.join(' ');
+    var pages = textToPrint.match(/.{1,15}/g);
+
+    jimp.read(fileName)
+        .then(function (image) {
+            processedImage = image;
+            return jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+        })
+        .then(function (font) {
+            var fileName = Math.floor(Date.now());
+            let xOffset = 25;
+            let yOffset = 75;
+            for (var i = 0; i < pages.length; ++i) {
+                processedImage.print(font, xOffset, yOffset, pages[i]);
+                yOffset += 32;
+                xOffset += 2;
+            }
+            var writtenPath = `./temp/${fileName}.png`;
+            var channel = message.channel; 
+            processedImage.write(writtenPath, function () {
+                channel.send('Here you go', {
+                    files: [
+                        writtenPath
+                    ]
+                });
+            }); 
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+module.exports.meta = {
+    action: "printimage",
+    active: true,
+    aliases: ["printimg"],
+    permissions: "ALL"
+}
+
+module.exports.help = function(pfx) {
+    var data = {
+        pretty: "Print on image",
+        description: "Prints your dumb text on image",
+        examples: `Type ${pfx}${this.meta.action} <text> to print it out`
+    };
+
+    return data;
+}
