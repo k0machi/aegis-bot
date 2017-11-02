76f8319db52fb5041e1a86eebe7458457ac78b19
Komachi <xenobyterus@gmail.com>
2017-11-02 20:37:04 +0300

    Trim whitespace from received realName field. Token is now discarded from database upon succesful verification.

--- commands/assignmember/index.js
+++ commands/assignmember/index.js
@@ -30,20 +30,19 @@
         const uuidv4 = require('uuid/v4');
         let token = uuidv4();
         ins = await bot.sql.run('INSERT INTO ' + tableName + '([discordid], [guildid], [token], [createdat]) VALUES (?, ?, ?, ?)', [discordId, guildId, token, Date.now()]);
-        const msg = await message.channel.send(`Here is your token: \`\`\`${token} \`\`\`\n\nGo to \"Edit Profile\", paste that into your profile's \"Real Name\" field and run \`$$verify ${steamUrl.replace("`", "")}\` again.`);
+        const msg = await message.channel.send(`Here is your token: \`\`\`${token}\`\`\`\n\nGo to \"Edit Profile\", paste that into your profile's \"Real Name\" field and run \`$$verify ${steamUrl.replace("`", "")}\` again.`);
     } else { //token data does exist, check user's profile
         var playerData = await getUserData(steamid);
         console.log(playerData.realname);
         console.log(tokenData.token);
-        if (tokenData.token == playerData.realname) {
+        if (tokenData.token == playerData.realname.trim()) {
             const msg = await message.channel.send("Token verified. You are now a member of following groups: " + groupsToAssign.join());
             for (let i = 0; i < groupsToAssign.length; i++)
             {
                 let role = await message.guild.roles.find('name', groupsToAssign[i]);
                 let rv = await message.member.addRole(role, `A new ${role.name} joins! ${message.author.username}`);
             }
-            //TODO: actually assign groups
-            //return false;
+            del = bot.sql.run(`DELETE FROM ${tableName} WHERE [token] = ?`, [tokenData.token]);
         } else {
             throw { message: "Token `"+tokenData.token+"` not found in profile" };
         }
