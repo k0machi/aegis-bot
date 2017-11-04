f3c5f9234692f80fd6a5b80354b21f0599e2b9c4
Komachi <xenobyterus@gmail.com>
2017-11-04 21:03:07 +0300

    Initial ES6 class refactor

--- aigis.js
+++ aigis.js
@@ -1,79 +1,143 @@
 ﻿const Phabricator = require("./commands/phabricator/phabricator.js");
-module.exports = {
-    debug: false,
-    client: null,
-    sql: null,
-    config: null,
-    canduit: null,
-    phabricator: null,
-    yaml: null,
-    fs: null,
-    pfx: "",
-    cooldowns: {},
-    commands: {},
-    aliases: {},
+const Discord = require("discord.js");
+const { promisify } = require("util");
+const directoryReader = promisify(require("fs").readdir);
+const sqlite = require("sqlite");
+const http = require("http");
+const fs = require("fs");
+const yaml = require("js-yaml");
+const qs = require("querystring");
+const canduit = require("canduit");
 
-    postPhabStory: function (post) {
-        var message = Object.create(this.phabricator.message_factory);
-        message.init(post, this);
-    },
-
-    init: function (config, sql, client, canduit, yaml, fs, process) {
+class Aigis 
+{
+    constructor() {
         if (process.argv.includes("--aidebug")) {
             this.debug = true;
             console.log("Debug mode");
         }
-        this.config = config;
-        this.client = client;
-        this.sql = sql;
-        this.yaml = yaml;
-        this.fs = fs;
-        this.pfx = this.config.command_prefix;
-        this.phabricator = Phabricator;
-        this.canduit = canduit;
-        this.phabricator.init(canduit, this);
-    },
+        //this.client = client;
+        //this.config = config;
+        //this.sql = sql;
+        //this.yaml = yaml;
+        //this.fs = fs;
+        //this.phabricator = Phabricator;
+        //this.canduit = canduit;
+        //
+        this.cooldowns = {};
+        this.commands = {};
+        this.aliases = {};
+    }
 
-    sayHello: async function (member) {
+    async Run() {
+        this.server = http.createServer((req, res) => {
+            res.end();
+        });
+        this.client = new Discord.Client();
+        sqlite.open("./database/main.db");
+        this.config = yaml.safeLoad(fs.readFileSync("./config.yml", "utf8"));
+        this.pfx = this.config.command_prefix;
+        this.canduit = canduit({ api: this.config.phab_host, user: "Aegis", token: this.config.phab_api_token }, () => { console.log("Conduit Init"); });
+        Phabricator.init(this.canduit, this);
+        const commands = await directoryReader("./commands/");
+    
+        commands.forEach(function(file) {
+            try {
+                var command = require(`./commands/${file}/index.js`);
+                console.log(`Loading command ${command.help(this.config.command_prefix).pretty}`);
+                var settings = yaml.safeLoad(fs.readFileSync(`./commands/${file}/command.yml`, "utf8"));
+                if (settings.active === false) {
+                    console.log(command.help(this.config.command_prefix).pretty + " is disabled, skipping...");
+                    return;
+                }
+                command.settings = settings;
+                this.registerCommand(command.meta.action, command);
+                let aliases = settings.aliases;
+                aliases.forEach((alias) => {
+                    this.registerAlias(alias, command.meta.action);
+                });
+            } catch (e) {
+                console.log(`Unable to parse files: ${file}: ${e}`);
+            }
+        }.bind(this));
+    
+        this.client.on("message", this.processCommand.bind(this));
+    
+        this.client.on("guildMemberAdd", function (member) {
+            try {
+                this.sayHello(member);
+            } catch (e) {
+                console.log(e.message);
+            }
+        }.bind(this));
+    
+        this.server.on("request", function(req, res) { //eslint-disable-line no-unused-vars
+            if (req.url.includes("phab-story")) {
+                if (this.debug) console.log("Endpoint phab-story called");
+                var body = "";
+                req.on("data", function (data) {
+                    body += data;
+                    if (body.length > 1e6)
+                        req.connection.destroy();
+                });
+    
+                req.on("end", function () {
+                    var post = qs.parse(body);
+                    this.postPhabStory(post);
+                });
+            }
+        }.bind(this));
+
+        this.server.listen(8888);
+        this.client.login(this.config.app_token);
+        this.setPresence();
+    }
+
+    async sayHello(member) {
         let wmsg = await this.sql.get("SELECT * FROM WelcomeMessages WHERE guildId == ?", [member.guild.id]);
         if (!wmsg) return;
         let channel = member.guild.channels.get(wmsg.ChannelId);
         if (!channel) throw { message: "SayHello(): Channel not found" };
         channel.send(member + ", " + wmsg.Message);
-    },
+    }
 
-    setPresence: function ()
-    {
+    postPhabStory(post) {
+        var message = Object.create(Phabricator.message_factory);
+        message.init(post, this);
+    }
+
+    setPresence() {
         this.client.on("ready", async () => {
             console.log("Initialization finished.");
             this.client.user.setPresence({ game: { name: `say ${this.pfx}aigis`, type: 0 } });
             if (this.debug) console.log(this.aliases);
         });
-    },
+    }
 
-    registerCommand: function (verb, object) {
+    registerCommand(verb, object) {
         this.commands[verb] = object;
-    },
+    }
 
-    registerAlias: function (alias, verb) {
+    registerAlias(alias, verb) {
         this.aliases[alias] = verb;
-    },
+    }
 
-    getTagMode: async function (guild) {
+    async getTagMode(guild) {
         let mode = await this.sql.get("SELECT * FROM TagModeData WHERE guildId == ?", [guild.id]);
         return mode.mode;
-    },
+    }
 
-    verifyPermission: function (member, perm) {
+    verifyPermission(member, perm) {
         let perms = member.hasPermission(perm, false, true);
         if (perms) {
             return true;
         } else {
             return false;
         }
-    },
-
-    processCommand: async function (message) {
+    }
+    
+    async processCommand(message) {
+        if (!message.content.startsWith(this.config.command_prefix)) return;
         const args = message.content.slice(this.config.command_prefix.length).trim().split(/ +/g);
         const command = args.shift();
         const cmd = this.commands[command] || this.commands[this.aliases[command]];
@@ -92,12 +156,14 @@ module.exports = {
             if (cd) throw {
                 message: `Please wait ${parseInt(cd /1000)} seconds before executing \`${this.pfx}${cmd.meta.action}\` again` 
             };
+            if (this.debug) console.log(`${command} exec`);
             await cmd.exec(this, message, args);
         } catch (e) {
             message.channel.send(e.message);
         }
-    },
-    checkCooldown: function (message, command) {
+    }
+
+    checkCooldown(message, command) {
         var cmd = this.commands[command] || this.commands[this.aliases[command]];
         var cmdName = cmd.meta.action;
         try {
@@ -119,9 +185,9 @@ module.exports = {
             if (!cd[message.member.user.id][cmdName]) cd[message.member.user.id][cmdName] = message.createdTimestamp;
             return false;
         }
-    },
+    }
 
-    switchTagMode: async function (guild, message) {
+    async switchTagMode(guild, message) {
         var mode = await this.sql.get("SELECT * FROM TagModeData WHERE guildId == ?", [guild.id]);
         if (mode) {
             await this.sql.run("UPDATE TagModeData SET [mode] = ? WHERE guildid == ?", [!mode.mode, mode.guildId]);
@@ -134,22 +200,22 @@ module.exports = {
             await this.sql.run("INSERT INTO TagModeData ([guildId], [mode]) VALUES (?, ?)", [guild.id, 1]);
             message.channel.send("Switched tagging mode to protected - only people with \"Manage Roles\" can create new tags");
         }
-    },
+    }
 
-    checkUserBlacklist: function (member) {
+    checkUserBlacklist(member) {
         if (member.roles.find("name", "Vandal")) return true;
         return false;
-    },
+    }
 
-    checkTag: async function (tagname, guild) {
+    async checkTag(tagname, guild) {
         let role_db = await this.sql.get("SELECT * FROM UserTags WHERE tagname == ?", [tagname]);
         let role_server = await guild.roles.find("name", tagname);
         if (role_db && role_server) return { "role": role_server, "db": role_db };
         if (!role_server && !role_db) return false;
         if (role_server && !role_db) throw { message: "Unmanaged role - not allowed!" };
-    },
+    }
 
-    createTag: async function (tagname, guild, user, time) {
+    async createTag(tagname, guild, user, time) {
         let role = await guild.createRole({
             name: tagname,
             color: "00e5ff",
@@ -159,10 +225,9 @@ module.exports = {
         let result = await this.sql.run("INSERT INTO UserTags ([tagname], [roleid], [guildid], [creatorid], [createdTimestamp]) VALUES (?,?,?,?,?)", [tagname, role.id, guild.id, user.id, time]);
         if (!result) throw { message: "Error accessing database" };
         return role;
-    },
-    
-    //TODO: Refactor argument list
-    deleteTag: async function (tagname, guild, channel, user) { //eslint-disable-line no-unused-vars
+    }
+
+    async deleteTag(tagname, guild, channel, user) { //eslint-disable-line no-unused-vars
         var result;
         try {
             let role = await this.checkTag(tagname, guild);
@@ -174,9 +239,9 @@ module.exports = {
         } catch (e) {
             channel.send(e.message);
         }
-    },
+    }
 
-    untagMe: async function (gname, user, guild, channel) {
+    async untagMe(gname, user, guild, channel) {
         try {
             let member = await guild.members.find("id", user.id);
             let role = await this.checkTag(gname, guild);
@@ -195,13 +260,18 @@ module.exports = {
             channel.send(e.message);
             console.log(e);
         }
-    },
+    }
 
-    logToDB: function (id, time, cmd, args, guild) {
+    logToDB(id, time, cmd, args, guild) {
         this.sql.run("INSERT INTO History ([User_Id], [Time], [Action], [Arguments], [Guild]) VALUES (?, ?, ?, ?, ?)", [id, time, cmd, JSON.stringify(args), guild.id]);
-    },
+    }
 
-    parseYAML: function (path) {
+    parseYAML(path) {
         return this.yaml.safeLoad(this.fs.readFileSync(path, "utf8"));
     }
-};
+}
+
+
+
+
+module.exports = Aigis;
